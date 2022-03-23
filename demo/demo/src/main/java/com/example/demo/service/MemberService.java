7c18440d6df1dbe0c2ae7254aa544f52fe821e2f
DDAMDOO <sjl9524@naver.com>
2022-03-23 17:39:46 +0900

    간단한 서비스 제작

---
+++ demo/demo/src/main/java/com/example/demo/service/MemberService.java
@@ -0,0 +1,52 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.Member;
+import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepo = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     * @param member
+     * @return id
+     */
+    public Long join(Member member) {
+
+//        Optional<Member> rst = memberRepo.findByName(member.getName());
+//
+//        rst.ifPresent(m -> {
+//            throw new IllegalStateException("member name already exist.");
+//        });
+        validateDuplicateMember(member);//중복 회원 인증
+
+        memberRepo.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepo.findAll();
+    }
+
+    /**
+     * 회원 조회
+     */
+    public Optional<Member> findOne(Long memberId){
+        return memberRepo.findById(memberId);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepo.findByName(member.getName())
+                        .ifPresent(m -> {
+                            throw new IllegalStateException("이미 존재하는 이름입니다.");
+                        });
+    }
+}
