bcd1dd8067cdf29d87f5c6bf4f8e08557090ac65
DDAMDOO <sjl9524@naver.com>
2022-03-27 14:09:43 +0900

    java bean을 통한 스프링 빈 등록하기

--- demo/demo/src/main/java/com/example/demo/service/MemberService.java
+++ demo/demo/src/main/java/com/example/demo/service/MemberService.java
@@ -2,22 +2,19 @@ package com.example.demo.service;
 
 import com.example.demo.domain.Member;
 import com.example.demo.repository.MemberRepository;
-import com.example.demo.repository.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
+
 public class MemberService {
     
     //private final MemberRepository memberRepo = new MemoryMemberRepository();//기존의 선언 방식
-    private final MemberRepository memberRepo;
+    private final MemberRepository memberRepository;
 
-    @Autowired
-    public MemberService(MemberRepository memberRepo) {
-        this.memberRepo = memberRepo;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     /**
@@ -34,7 +31,7 @@ public class MemberService {
 //        });
         validateDuplicateMember(member);//중복 회원 인증
 
-        memberRepo.save(member);
+        memberRepository.save(member);
         return member.getId();
     }
 
@@ -42,18 +39,18 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepo.findAll();
+        return memberRepository.findAll();
     }
 
     /**
      * 회원 조회
      */
     public Optional<Member> findOne(Long memberId){
-        return memberRepo.findById(memberId);
+        return memberRepository.findById(memberId);
     }
 
     private void validateDuplicateMember(Member member) {
-        memberRepo.findByName(member.getName())
+        memberRepository.findByName(member.getName())
                         .ifPresent(m -> {
                             throw new IllegalStateException("이미 존재하는 이름입니다.");
                         });
